5aa0dc10196a486f34dc818cbc3a6c61f4927c9d
LauritsSchou <laurits.schou@gmail.com>
2023-02-20 22:23:05 +0100

    javascript robber1

--- script.js
+++ script.js
@@ -9,4 +9,12 @@ function die() {
     .querySelector("#robber1_container")
     .removeEventListener("click", die);
   document.querySelector("#robber1_container").classList.add("die");
+  document
+    .querySelector("#robber1_container")
+    .addEventListener("animationend", dead);
+}
+function dead() {
+  console.log("dead");
+  document.querySelector("#robber1_container").classList.remove("die");
+  document.querySelector("#robber1_container").addEventListener("click", die);
 }
