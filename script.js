8ae9e64094bdf4d84692424fd325f50605c78e79
LauritsSchou <laurits.schou@gmail.com>
2023-02-21 08:50:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ function die() {
   document
     .querySelector("#robber1_container")
     .removeEventListener("click", die);
-  document.querySelector("#robber1_container").classList.add("die");
+  document.querySelector("#robber1_sprite").classList.add("die");
+  document.querySelector("#robber1_container").classList.add("paused");
   document
     .querySelector("#robber1_container")
     .addEventListener("animationend", dead);
 }
 function dead() {
   console.log("dead");
-  document.querySelector("#robber1_container").classList.remove("die");
+  document.querySelector("#robber1_sprite").classList.remove("die");
+  document.querySelector("#robber1_container").classList.remove("paused");
   document.querySelector("#robber1_container").addEventListener("click", die);
 }
