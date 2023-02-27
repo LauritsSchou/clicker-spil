e6dcfa82668ea573a66050180c7418bae37fcba7
LauritsSchou <laurits.schou@gmail.com>
2023-02-27 20:33:59 +0100

    this og random positions + flere elements

--- script.js
+++ script.js
@@ -6,18 +6,102 @@ let lives = 3;
 
 function start() {
   console.log("start");
+  startAnimation();
+  addPosition();
+  registerClicks();
+  animationRestart();
+}
+function startAnimation() {
   document.querySelector("#robber1_container").classList.add("robber1_walk");
+  document.querySelector("#robber2_container").classList.add("robber1_walk");
+  document.querySelector("#robber3_container").classList.add("robber1_walk");
   document.querySelector("#civilian1_container").classList.add("civilian1_walk");
+  document.querySelector("#civilian2_container").classList.add("civilian1_walk");
+  document.querySelector("#civilian3_container").classList.add("civilian1_walk");
+}
+function addPosition() {
+  document.querySelector("#robber1_container").classList.add("position1");
+  document.querySelector("#robber2_container").classList.add("position2");
+  document.querySelector("#robber3_container").classList.add("position3");
+  document.querySelector("#civilian1_container").classList.add("position4");
+  document.querySelector("#civilian2_container").classList.add("position5");
+  document.querySelector("#civilian3_container").classList.add("position6");
+}
+function registerClicks() {
+  console.log("registerclicks");
   document.querySelector("#robber1_container").addEventListener("click", dieRobber);
+  document.querySelector("#robber2_container").addEventListener("click", dieRobber);
+  document.querySelector("#robber3_container").addEventListener("click", dieRobber);
   document.querySelector("#civilian1_container").addEventListener("click", dieCivilian);
+  document.querySelector("#civilian2_container").addEventListener("click", dieCivilian);
+  document.querySelector("#civilian3_container").addEventListener("click", dieCivilian);
+}
+function animationRestart() {
+  document.querySelector("#robber1_container").addEventListener("animationiteration", robberRestart);
+  document.querySelector("#robber2_container").addEventListener("animationiteration", robberRestart);
+  document.querySelector("#robber3_container").addEventListener("animationiteration", robberRestart);
+  document.querySelector("#civilian1_container").addEventListener("animationiteration", civilianRestart);
+  document.querySelector("#civilian2_container").addEventListener("animationiteration", civilianRestart);
+  document.querySelector("#civilian3_container").addEventListener("animationiteration", civilianRestart);
 }
 function dieRobber() {
   console.log("dieRobber");
-  document.querySelector("#robber1_container").removeEventListener("click", dieRobber);
-  document.querySelector("#robber1_container").classList.add("paused");
-  document.querySelector("#robber1_sprite").classList.add("rotate");
-  document.querySelector("#robber1_container").addEventListener("animationend", deadRobber);
+  let robber = this;
+  robber.removeEventListener("click", dieRobber);
+  robber.classList.add("paused");
+  robber.querySelector("img").classList.add("rotate");
+  robber.addEventListener("animationend", deadRobber);
   incrementPoints();
+}
+
+function deadRobber() {
+  console.log("deadrobber");
+  let robber = this;
+  robber.removeEventListener("animationend", deadRobber);
+  robber.querySelector("img").classList.remove("rotate");
+  robber.classList.remove("paused");
+  robberRestart.call(this);
+  robber.addEventListener("click", dieRobber);
+}
+function robberRestart() {
+  console.log("robberrestart");
+  let robber = this;
+  robber.classList.remove("robber1_walk");
+  robber.offsetWidth;
+  robber.classList.add("robber1_walk");
+  robber.classList.remove("position1", "position2", "position3", "position4", "position5", "position6");
+  let pos = Math.floor(Math.random() * 6) + 1;
+  robber.classList.add("position" + pos);
+}
+
+function dieCivilian() {
+  console.log("dieCivilian");
+  let civilian = this;
+  civilian.removeEventListener("click", dieCivilian);
+  civilian.classList.add("paused");
+  civilian.querySelector("img").classList.add("rotate");
+  civilian.addEventListener("animationend", deadCivilian);
+  decrementLives();
+}
+
+function deadCivilian() {
+  let civilian = this;
+  console.log("deadCivilian");
+  civilian.removeEventListener("animationend", deadCivilian);
+  civilian.querySelector("img").classList.remove("rotate");
+  civilian.classList.remove("paused");
+  civilianRestart.call(this);
+  civilian.addEventListener("click", dieCivilian);
+}
+function civilianRestart() {
+  console.log("civilianrestart");
+  let civilian = this;
+  civilian.classList.remove("civilian1_walk");
+  civilian.offsetWidth;
+  civilian.classList.add("civilian1_walk");
+  civilian.classList.remove("position1", "position2", "position3", "position4", "position5", "position6");
+  let pos = Math.floor(Math.random() * 6) + 1;
+  civilian.classList.add("position" + pos);
 }
 function incrementPoints() {
   points++;
@@ -28,25 +112,6 @@ function incrementPoints() {
       levelComplete();
     }
   }
-}
-function deadRobber() {
-  console.log("deadRobber");
-  document.querySelector("#robber1_container").removeEventListener("animationend", deadRobber);
-  document.querySelector("#robber1_sprite").classList.remove("rotate");
-  document.querySelector("#robber1_container").classList.remove("paused");
-  document.querySelector("#robber1_container").classList.remove("robber1_walk");
-  document.querySelector("#robber1_container").offsetWidth;
-  document.querySelector("#robber1_container").classList.add("robber1_walk");
-  document.querySelector("#robber1_container").addEventListener("click", dieRobber);
-}
-
-function dieCivilian() {
-  console.log("dieCivilian");
-  document.querySelector("#civilian1_container").removeEventListener("click", dieCivilian);
-  document.querySelector("#civilian1_sprite").classList.add("rotate");
-  document.querySelector("#civilian1_container").classList.add("paused");
-  document.querySelector("#civilian1_container").addEventListener("animationend", deadCivilian);
-  decrementLives();
 }
 function decrementLives() {
   lives--;
@@ -61,16 +126,6 @@ function displayDecrementLives() {
   document.querySelector(`#heart${lives + 1}`).classList.add("broken_heart");
 }
 
-function deadCivilian() {
-  console.log("deadCivilian");
-  document.querySelector("#civilian1_container").removeEventListener("animationend", deadCivilian);
-  document.querySelector("#civilian1_sprite").classList.remove("rotate");
-  document.querySelector("#civilian1_container").classList.remove("paused");
-  document.querySelector("#civilian1_container").classList.remove("civilian1_walk");
-  document.querySelector("#civilian1_container").offsetWidth;
-  document.querySelector("#civilian1_container").classList.add("civilian1_walk");
-  document.querySelector("#civilian1_container").addEventListener("click", dieCivilian);
-}
 function levelComplete() {
   document.querySelector("#level_complete").classList.remove("hidden");
   document.querySelector("#levelCompleteMessage").textContent = `Congratulations. You killed ${points} robbers`;
